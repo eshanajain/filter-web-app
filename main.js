6ee318e977a3aeca9f7cc7aee6547e383c623221
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-12-23 15:37:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,30 @@ function preload() {
 function setup() {
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEI);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet is Initialized');
 }
 function draw() {
 }
 
 function take_snapshot() {
     save('myfilterimage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("lip x = " + results[0].pose.lip.x);
+        console.log("lip y = " + results[0].pose.lip.y);
+    }
 }
